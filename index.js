00ddb8dea1fa2517db1af4f2a8a462b03baa1a14
kaprestridge <kaprestridge@gmail.com>
2021-07-29 12:58:22 +1000

    takes time input and takes away 45 minutes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-export const returnTrue = () => true
+var startTime = document.getElementById("startTime");
+var valueSpan = document.getElementById("value");
+
+startTime.addEventListener("input", function() {
+  valueSpan.innerText = minusTime(startTime.value);
+}, false);
+
+
+const minusTime = (time) => {
+    let timeArray = time.split(':')
+    let hours = timeArray[0]
+    let minutes = timeArray[1]
+    let amOrPm = "am"
+    if (timeArray[1] >= 45){
+        minutes = timeArray[1] - 45
+    } else {
+        hours = timeArray[0] - 1
+        minutes = parseInt(timeArray[1]) + 15
+    }
+
+    if (hours > 12){
+        hours = hours - 12
+        amOrPm = "pm"
+    }
+
+    if (minutes < 10){
+        minutes = '0' + minutes
+    }
+    return (hours + ":" + minutes + " " + amOrPm)
+}
